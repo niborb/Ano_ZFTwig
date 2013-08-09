eb26a9626993cb2c4ed4ea5fb3bec00e8bc3da8b
Arno Geurts - Werk <arnogeurts-werk@arno.jb-it.local>
2013-08-09 16:10:44 +0200

    Bugfix for 'isset' in partial in the PHP engine

--- library/Ano/View/Engine/PhpEngine.php
+++ library/Ano/View/Engine/PhpEngine.php
@@ -161,6 +161,9 @@ class Ano_View_Engine_PhpEngine extends Ano_View_Engine_Abstract
      */
     public function __isset($key)
     {
-        return $this->getView()->__isset($key);
+    public function __isset($key)
+    {
+        return $this->getView()->__isset($key) || array_key_exists($key, $this->_tempVars);
+    }
     }
 }
